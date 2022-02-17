f30b96dc43f7c1e3f6bb459aa0028c14003e8b12
Ice Tam <icetam4@gmail.com>
2022-02-17 17:05:17 -0500

    updates

--- server.js
+++ server.js
@@ -18,63 +18,6 @@ app.use('/customers', require('./customers.routes'));
 app.use('/orders', require ('./orders.routes'));
 
 
-// app.get('/orders/:id', async(req, res, next) => {
-//   try {
-//     const orders = await Order.findByPk(req.params.id, {
-//       include: [ Customer ]
-//    });
-
-//    const html = orders.customers.map( customer=> {
-//     return `
-//     <div>
-//     <ul>
-//       <li>  ${customer.name} </li>
-//     </ul>
-//     <form method='POST' action='/customers/${customer.id}?_method=delete'>
-//     <button class='button2'> Order Completed </button>
-//     </form>
-//     </div>
-
-
-//     `;
-// }).join('');
-
-
-// res.send(`
-//  <html>
-//    <head> 
-//     <link rel = 'stylesheet' href='/public/style.css'/> 
-//     <title> Fish Cheeks Orders </title>
-//    </head>
-//    <body>
-     
-//       <h1 class='header'> Order Fulfillment </h1>
-//       <a class='link' href= '/customers'> < Full List of Orders </a>
-//        <h1 class='name'>
-//         ${orders.name}
-//         </h1>
-//         <div class='order2'>
-//        ${html}
-//        </div>
-     
-//    </body>
-//  </html>
-// `)
-
-// }
-//   catch (ex) {
-//     next(ex)
-//   }
-
-
-// });
-
-
-
-
-
-
-
 const start = async () => {
     try{
 
